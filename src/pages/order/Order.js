009a54595e5613d84762034ef68c72960698f9ee
sambathFerns <sambath.ferns@gmail.com>
2024-04-19 17:27:24 +0530

    design corrections

--- src/pages/order/Order.js
+++ src/pages/order/Order.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useContext, useEffect, useState } from "react";
 import * as Md from "react-icons/md";
 import * as Pi from "react-icons/pi";
+import * as Fa from "react-icons/fa";
+import * as Ti from "react-icons/ti";
 import { AppContext } from "../../context/AppContext";
 
 function Order() {
@@ -8,6 +10,10 @@ function Order() {
   const [selectedItem, setSelectedItem] = useState(null);
   const [delivery, setDelivey] = useState(false);
   const [filteredList, setFilteredList] = useState(null);
+  const [activeChipIndex, setActiveChipIndex] = useState(0);
+  const [itemCount, setItemCount] = useState(1);
+  const [added, setAdded] = useState(false);
+  const [deliveryOption, setDeliveryOption] = useState("delivery");
 
   useEffect(() => {
     if (!menuList) return;
@@ -51,21 +57,32 @@ function Order() {
     }
   };
   const menuItems = [
+    "STARTERS & SIDE DISHES",
     "PIZZA",
     "ITALIAN PASTA",
     "PIZZA & PASTA DEAL",
     "SALADS",
     "BURGERS",
     "BURGER MEALS",
-    "STARTERS & SIDE DISHES",
     "DRINKS",
     "DESSERTS",
     "ICE CREAM",
     "SPECIAL OFFERS",
   ];
-  const moveLeft = () => {};
+  function moveLeft() {
+    const menuList = document.querySelector(".menu-list");
+    const firstItem = menuList.querySelector("li:first-child");
 
-  const moveRight = () => {};
+    menuList.appendChild(firstItem);
+  }
+
+  function moveRight() {
+    const menuList = document.querySelector(".menu-list");
+    const lastItem = menuList.querySelector("li:last-child");
+
+    menuList.insertBefore(lastItem, menuList.firstChild);
+  }
+
   return (
     <Fragment>
       <section className="order-online">
@@ -79,15 +96,153 @@ function Order() {
           </p>
 
           <div className="chip-container mt-4 d-flex">
-            <button onClick={moveLeft}>&lt;</button>
+            <button className="nav-btn left" onClick={moveLeft}>
+              <i>
+                <Fa.FaArrowLeft />
+              </i>
+            </button>
             <ul className="menu-list ">
               {menuItems.map((item, index) => (
-                <li key={index}>
+                <li
+                  className={index === activeChipIndex ? "active" : ""}
+                  key={index}
+                  onClick={() => setActiveChipIndex(index)}
+                >
                   {item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()}
                 </li>
               ))}
             </ul>
-            <button onClick={moveRight}>&gt;</button>
+            <button className="nav-btn right" onClick={moveRight}>
+              <i>
+                <Fa.FaArrowRight />
+              </i>
+            </button>
+          </div>
+        </div>
+        <div className="wrapper">
+          <div className="container">
+            <div className="food_order_area">
+              <div className="order_block">
+                <div className="row">
+                  <div className="col-lg-12 col-md-12 col-sm-12 mt-3">
+                    <div className="card food-card">
+                      <div className="row">
+                        <div className="col-lg-4 col-md-4 col-sm-4">
+                          <div className="food-img">
+                            <img
+                              src={require("../../assets/images/food.jpg")}
+                              alt=""
+                            />
+                          </div>
+                        </div>
+                        <div className="col-lg-8 col-md-8 col-sm-8">
+                          <div className="food_content position-relative">
+                            <h3>Russian Delight</h3>
+                            <p className="rest_name">Restaurent Name</p>
+                            <p className="desc_">
+                              Lorem ipsum dolor sit amet consectetur adipisicing
+                              elit. Omnis nam quidem earum voluptatibus sunt
+                              iusto praesentium, eius sapiente provident
+                              corrupt. Lorem ipsum dolor sit amet consectetur
+                              adipisicing elit. Error odio illo ipsa, inventore
+                              quam omnis suscipit, quod est fugiat laborum
+                              voluptate, architecto velit dicta reprehenderit
+                              debitis enim perferendis dolore voluptatum?
+                            </p>
+                            <i className="food_type">
+                              <div class="box">
+                                <div class="circle"></div>
+                              </div>
+                            </i>
+                            <p className="price_">£190</p>
+                            <div className="wrapper__">
+                              <div className="inc_dec_wrapper">
+                                <button
+                                  class="decrement"
+                                  onClick={() => {
+                                    itemCount >= 2 &&
+                                      setItemCount(itemCount - 1);
+                                  }}
+                                >
+                                  -
+                                </button>
+                                <p>{itemCount}</p>
+                                <button
+                                  class="increment"
+                                  onClick={() => setItemCount(itemCount + 1)}
+                                >
+                                  +
+                                </button>
+                              </div>
+                              <button
+                                type="button"
+                                className={
+                                  !added ? "order_now" : "order_now added"
+                                }
+                                onClick={() => setAdded(!added)}
+                                disabled={added}
+                              >
+                                {added ? <Fa.FaCheck /> : <Ti.TiPlus />}
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="billing_block bill-spikes">
+                <h3 className="order_title">Order Summary</h3>
+                <div className="summary_card card">
+                  <p className="food_menu">Food Name</p>
+                  <p className="price_summary_1">£190</p>
+
+                  <button type="button" className="remove">
+                    <Fa.FaRegTrashAlt />
+                  </button>
+                </div>
+                <br />
+                <div className="line__"></div>
+                <div className="d-flex mt-4">
+                  <label
+                    htmlFor="delivery"
+                    className="delivery_option_container"
+                  >
+                    <input
+                      type="radio"
+                      name="deliveryOption"
+                      id="delivery"
+                      className="delivery_option"
+                      checked={deliveryOption === "delivery"}
+                      onClick={() => setDeliveryOption("delivery")}
+                    />
+                    <span class="checkmark"></span>
+                    Delivery
+                  </label>
+                  <label
+                    htmlFor="takeAway"
+                    className="delivery_option_container"
+                  >
+                    <input
+                      type="radio"
+                      name="deliveryOption"
+                      id="takeAway"
+                      className="ms-3 delivery_option"
+                      onClick={() => setDeliveryOption("takeaway")}
+                      checked={deliveryOption === "takeaway"}
+                    />
+                    <span class="checkmark"></span>
+                    Take away
+                  </label>
+                </div>
+                <br />
+                <label htmlFor="" className="opt_label_827">
+                  Postal Code
+                </label>
+                <input type="text" name="" id="" className="opt_input_827" />
+              </div>
+            </div>
           </div>
         </div>
       </section>
