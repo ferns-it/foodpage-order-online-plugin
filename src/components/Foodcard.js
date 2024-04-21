56f2d12cb97dd882b3dc637d76ea45e12f554733
sambathFerns <sambath.ferns@gmail.com>
2024-04-21 23:36:38 +0530

    UI and corrections

---
+++ src/components/Foodcard.js
@@ -0,0 +1,102 @@
+import React, { Fragment, useContext, useEffect, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import * as Bs from "react-icons/bs";
+
+function Foodcard() {
+  const {
+    menuList,
+    menuLoading,
+    categoryList,
+    fetchProductsList,
+    productsList,
+  } = useContext(AppContext);
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    if (!categoryList || categoryList.length === 0) return;
+
+    const fetchData = async () => {
+      const pro = await Promise.all(
+        categoryList.map(async (item) => {
+          const data = {
+            shopId: 1,
+            categoryId: item?.cID,
+          };
+
+          const productRespo = await fetchProductsList(data);
+          return { categoryName: item?.name, product: productRespo };
+        })
+      );
+      setProducts(pro);
+    };
+
+    fetchData();
+  }, [categoryList]);
+  return (
+    <Fragment>
+      {products &&
+        products.length != 0 &&
+        products.map((list, key) => {
+          const productData = list?.product;
+          return (
+            <div className="product_wrapper_029" id={`category-${key}`}>
+              <h3 className="cat_2901"> {list?.categoryName ?? "N/A"}</h3>
+              <br />
+              <div className="row">
+                {productData &&
+                  productData.length != 0 &&
+                  productData.map((product, index) => {
+                    console.log("product", product);
+                    return (
+                      <div className="col-lg-6 col-md-6 col-sm-6">
+                        <div className="food_card_wrapper_029">
+                          <div className="upper_pot_029 position-relative">
+                            <div className="food_img_029">
+                              <img src={product?.photo} alt="" />
+                            </div>
+                            <div className="food_type">
+                              <div
+                                className={
+                                  product?.type === "veg"
+                                    ? "box veg"
+                                    : product?.type === "non veg"
+                                    ? "box non"
+                                    : "box unkown"
+                                }
+                              >
+                                <div
+                                  className={
+                                    product?.type === "veg"
+                                      ? "circle veg"
+                                      : product?.type === "non veg"
+                                      ? "circle non"
+                                      : "circle unkown"
+                                  }
+                                ></div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="bottom_pot_029">
+                            <h3 className="food_name_029">
+                              {product?.name ?? "N/A"}
+                            </h3>
+                            <p className="food_desc_029">
+                              {product?.description ?? "N/A"}
+                            </p>
+                            <button type="button" className="cart_btn_029">
+                              <Bs.BsCart3 /> <span>add cart</span>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+          );
+        })}
+    </Fragment>
+  );
+}
+
+export default Foodcard;
--- src/pages/order/Order.js
+++ src/pages/order/Order.js
@@ -6,6 +6,7 @@ import * as Ti from "react-icons/ti";
 import * as Lu from "react-icons/lu";
 import * as Bs from "react-icons/bs";
 import { AppContext } from "../../context/AppContext";
+import Foodcard from "../../components/Foodcard";
 
 function Order() {
   const {
@@ -15,14 +16,11 @@ function Order() {
     fetchProductsList,
     productsList,
   } = useContext(AppContext);
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [delivery, setDelivey] = useState(false);
   const [filteredList, setFilteredList] = useState(null);
   const [activeChipIndex, setActiveChipIndex] = useState(-1);
-  const [itemCount, setItemCount] = useState(1);
   const [added, setAdded] = useState(false);
   const [deliveryOption, setDeliveryOption] = useState("delivery");
-  const [products, setProducts] = useState(null);
+  
 
   useEffect(() => {
     if (!menuList) return;
@@ -37,77 +35,35 @@ function Order() {
     setFilteredList(data);
   }, [menuList]);
 
-  useEffect(() => {
-    if (!categoryList || categoryList.length === 0) return;
+  // useEffect(() => {
+  //   if (!categoryList || categoryList.length === 0) return;
 
-    const fetchData = async () => {
-      const pro = await Promise.all(
-        categoryList.map(async (item) => {
-          const data = {
-            shopId: 1,
-            categoryId: item?.cID,
-          };
+  //   const fetchData = async () => {
+  //     const pro = await Promise.all(
+  //       categoryList.map(async (item) => {
+  //         const data = {
+  //           shopId: 1,
+  //           categoryId: item?.cID,
+  //         };
 
-          const productRespo = await fetchProductsList(data);
-          return { categoryName: item?.name, product: productRespo };
-        })
-      );
-      setProducts(pro);
-    };
+  //         const productRespo = await fetchProductsList(data);
+  //         return { categoryName: item?.name, product: productRespo };
+  //       })
+  //     );
+  //     setProducts(pro);
+  //   };
 
-    fetchData();
-  }, [categoryList]);
+  //   fetchData();
+  // }, [categoryList]);
 
-  // console.log("products", products);
-
-  // fetch("https://foodpage.co.uk/development/v2/shop/products/1/0")
-  //   .then((response) => {
-  //     if (!response.ok) {
-  //       throw new Error("Network response was not ok");
-  //     }
-  //     return response.json();
-  //   })
-  //   .then((data) => {
-  //     // Handle the JSON data received from the server
-  //     console.log(data);
-  //   })
-  //   .catch((error) => {
-  //     // Handle any errors that occurred during the fetch
-  //     console.error("Fetch error:", error);
-  //   });
-
-  const handleKeyPress = (event) => {
-    // Prevent input of negative symbol (-) if pressed
-    if (event.key === "-" || event.key === "e") {
-      event.preventDefault();
+  const handleChipClick = (index) => {
+    setActiveChipIndex(index);
+    
+    const sectionId = document.getElementById(`category-${index}`);
+    if (sectionId) {
+      sectionId.scrollIntoView({ behavior: "smooth", block: "start" });
     }
   };
-  const menuItems = [
-    "STARTERS & SIDE DISHES",
-    "PIZZA",
-    "ITALIAN PASTA",
-    "PIZZA & PASTA DEAL",
-    "SALADS",
-    "BURGERS",
-    "BURGER MEALS",
-    "DRINKS",
-    "DESSERTS",
-    "ICE CREAM",
-    "SPECIAL OFFERS",
-  ];
-  function moveLeft() {
-    const menuList = document.querySelector(".menu-list");
-    const firstItem = menuList.querySelector("li:first-child");
-
-    menuList.appendChild(firstItem);
-  }
-
-  function moveRight() {
-    const menuList = document.querySelector(".menu-list");
-    const lastItem = menuList.querySelector("li:last-child");
-
-    menuList.insertBefore(lastItem, menuList.firstChild);
-  }
 
   return (
     <Fragment>
@@ -126,7 +82,7 @@ function Order() {
             <div className="food_order_area">
               <div className="order_block">
                 <div className="row">
-                  <div className="col-lg-4 col-md-4 col-sm-4">
+                  <div className="col-lg-3 col-md-3 col-sm-3">
                     <div className="card category_card_009 p-2">
                       <ul className="food_category_009">
                         {categoryList &&
@@ -134,14 +90,14 @@ function Order() {
                           categoryList.map((list, index) => {
                             return (
                               <a
-                                href={`#${list?.name}`}
+                                href={`#category-${index}`}
                                 className={
                                   index === activeChipIndex
                                     ? "nav-link active_009"
                                     : "nav-link"
                                 }
                                 key={index}
-                                onClick={() => setActiveChipIndex(index)}
+                                onClick={() => handleChipClick(index)}
                               >
                                 <li>{list?.name}</li>
                                 <i>
@@ -153,52 +109,8 @@ function Order() {
                       </ul>
                     </div>
                   </div>
-                  <div className="col-lg-8 col-md-8 col-sm-8">
-                    {products &&
-                      products.length != 0 &&
-                      products.map((list, key) => {
-                        const productData = list?.product;
-                        return (
-                          <div className="product_wrapper_029" key={key}>
-                            <h3 className="cat_2901">
-                              {list?.categoryName ?? "N/A"}
-                            </h3>
-                            <div className="row">
-                              {productData &&
-                                productData.length != 0 &&
-                                productData.map((product, index) => {
-                                  return (
-                                    <div className="col-lg-6 col-md-6 col-sm-6">
-                                      <div className="food_card_029">
-                                        <div className="prod_img_029">
-                                          <img src={product?.photo} alt="" />
-                                        </div>
-                                        <div className="food_content_029">
-                                          <div className="content_029">
-                                            <p className="food_name_029">
-                                              {product?.name ?? "N/A"}
-                                            </p>
-                                            <p className="desc_029">
-                                              {product?.description ?? "N/A"}
-                                            </p>
-                                            {/* <button type="button" className="buy_now_029">Buy now</button> */}
-                                          </div>
-
-                                        </div>
-                                          <button
-                                            type="button"
-                                            className="cart_btn_029"
-                                          >
-                                            <Bs.BsCart3 />
-                                          </button>
-                                      </div>
-                                    </div>
-                                  );
-                                })}
-                            </div>
-                          </div>
-                        );
-                      })}
+                  <div className="col-lg-9 col-md-9 col-sm-9">
+                    <Foodcard />
                   </div>
                 </div>
               </div>
