1297f5ab405c950440b8f224b65244617172e8b4
sambathFerns <sambath.ferns@gmail.com>
2024-05-03 18:00:41 +0530

    changes

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -66,6 +66,42 @@ function OrderSummary() {
     // sessionStorage.setItem("locationDetails", JSON.stringify(locationResponse));
   };
 
+  const isValidRadiusCheck = () => {
+    if (!locationResponse || !settings || !settings.deliveryInfo) return;
+
+    const deliveryData = settings?.deliveryInfo;
+    if (!deliveryData) return;
+
+    let distanceType = deliveryData?.distanceType;
+    let freeDelivery = parseInt(deliveryData?.freeDelivery);
+    let freeDeliveryRadius = parseInt(deliveryData?.freeDeliveryRadius);
+    let deliveryChargeType = deliveryData?.deliveryChargeType;
+    let ratePerMile = deliveryData?.ratePerMile;
+    let customerDistance =
+      locationResponse?.rows[0]?.elements[0]?.distance?.value;
+
+    let km = customerDistance / 1000;
+    let distanceInMiles = km * 0.621371;
+    let roundedDistanceInMiles = parseInt(distanceInMiles.toFixed(2));
+
+    let freeDeliveryCondition =
+      freeDelivery == 1 && roundedDistanceInMiles < freeDeliveryRadius;
+
+    if (freeDeliveryCondition === true) {
+      setIsFreeDelivery(true);
+      setDeliveryCharges({ charge: 0, ratePerMile: 0 });
+    } else {
+      setIsFreeDelivery(false);
+    }
+    
+    if (isFreeDelivery == false) {
+      console.log("reached false");
+      const excessDistance = distanceInMiles - freeDeliveryRadius;
+      const deliveryCharge = excessDistance * ratePerMile;
+      setDeliveryCharges({ charge: deliveryCharge.toFixed(2), ratePerMile });
+    }
+  };
+
   const toggleFoodLists = (index) => {
     if (index < 0) return;
     setShowAddons((prevList) => {
@@ -103,7 +139,7 @@ function OrderSummary() {
         setError(true);
         return;
       }
-
+      isValidRadiusCheck();
       console.log(isFreeDelivery, deliveryCharges);
       const postCodeValidation = handleAddress();
 
