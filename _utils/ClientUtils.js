e985f95cd8c2581a20559461445c58ffad2bc98a
sambathFerns <sambath.ferns@gmail.com>
2024-10-16 13:45:34 +0530

    utils added

---
+++ _utils/ClientUtils.js
@@ -0,0 +1,48 @@
+export const isClient = typeof window !== "undefined";
+
+export const getSessionStorageItem = (key) => {
+  if (isClient) {
+    return sessionStorage.getItem(key);
+  }
+  return null;
+};
+
+export const setSessionStorageItem = (key, value) => {
+  if (isClient) {
+    sessionStorage.setItem(key, value);
+  }
+};
+
+export const removeSessionStorageItem = (key) => {
+  if (isClient) {
+    sessionStorage.removeItem(key);
+  }
+};
+
+export const removeLocalStorageItem = (key) => {
+  if (isClient) {
+    const value = localStorage.getItem(key);
+    if (value) {
+      localStorage.removeItem(key);
+    }
+  }
+};
+
+export const getLocalStorageItem = (key) => {
+  if (isClient) {
+    return localStorage.getItem(key);
+  }
+  return null;
+};
+
+export const setLocalStorageItem = (key, value) => {
+  if (isClient) {
+    localStorage.setItem(key, value);
+  }
+};
+
+export const redirectToLocation = (url) => {
+  if (isClient) {
+    window.location.href = url;
+  }
+};
