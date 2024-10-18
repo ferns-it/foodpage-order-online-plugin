22026e8117436270d70a1f1bef796a3f7cea0713
sambathFerns <sambath.ferns@gmail.com>
2024-10-18 11:18:12 +0530

    payment intent payload changes

--- order-online-page/components/OrderSummaryCheckout.js
+++ order-online-page/components/OrderSummaryCheckout.js
@@ -218,10 +218,13 @@ function OrderSummaryCheckout() {
           User: userID,
         };
         setIntentLoading(true);
+        const discountam = Number(details?.discount ?? paramsValues.discount);
+        const delivFeeAmt = deliveryFee ?? paramsValues?.deliveryFee;
+
         await createPaymentIntent(
           {
-            devliveryCharges: deliveryFee ?? 0 * 100,
-            discountAmount: discount * 100,
+            devliveryCharges: (delivFeeAmt ?? 0) * 100,
+            discountAmount: isNaN(discountam) ? 0 : parseInt(discountam * 100),
             shopID: shopId,
           },
 
