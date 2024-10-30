3837a730015026d2f90872f7ba4e7cf5391810ea
sambathFerns <sambath.ferns@gmail.com>
2024-10-30 17:31:50 +0530

    corrections

--- guest-login/OtpVerification.jsx
+++ guest-login/OtpVerification.jsx
@@ -65,8 +65,8 @@ function OtpVerification() {
     const header = {
       alg: "FP2024",
       typ: "JWT",
-      exp: Math.floor(Date.now() / 1000) + (5 * 60),//! one min
-      // exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60), //! one day
+      // exp: Math.floor(Date.now() / 1000) + (5 * 60),//! one min
+      exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60), //! one day
     };
     const encodedHeaders = btoa(JSON.stringify(header));
     const encodedPayload = btoa(JSON.stringify(payload));
