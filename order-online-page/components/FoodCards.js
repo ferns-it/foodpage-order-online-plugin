23b4b47758871b7bc47ce2f2fef6bc7cfa277ebf
parvathygopan <parvathy671999@gmail.com>
2024-11-14 14:44:25 +0530

    plugin details added

--- order-online-page/components/FoodCards.js
+++ order-online-page/components/FoodCards.js
@@ -32,8 +32,7 @@ function FoodCards(category) {
 
     if (showModal) {
       const data = getSessionStorageItem("selectedProduct");
-
-      window.sessionStorage.removeItem("selectedProduct");
+      console.log(data, "data");
       if (data && data.length != 0) {
         const product = JSON.parse(data);
         setShowModal(true);
