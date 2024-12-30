696cceb07a1eba49d16506a444d5a78fc27d7c43
Reshmaalan2001 <reshmaalan01@gmail.com>
2024-12-30 12:18:58 +0530

    Assigning shopid to env in api

--- order-online-page/constants/APIEndpoints.js
+++ order-online-page/constants/APIEndpoints.js
@@ -1,8 +1,8 @@
 "use client";
 
-const id = 1;
+const id = process.env.SHOP_ID;
 const shopId = `${id}-shop`;
-const shopURL = "le-arabia";
+const shopURL = process.env.SHOP_URL;
 
 export const APIEndpoints = {
   menulist: `products/${id}/0`,
