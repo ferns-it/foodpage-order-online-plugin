6fcbc9396a52a51bad8c9c41a9531903d2477897
parvathygopan <parvathy671999@gmail.com>
2024-12-31 10:16:33 +0000

    table reservation changes

--- table-reservation/context/TableReservationContext.js
+++ table-reservation/context/TableReservationContext.js
@@ -27,7 +27,7 @@ export const TableReservationContextProvider = (props) => {
       setLoading(true);
       try {
         const response = await fetch(
-          `https://shopadmin.vgrex.com/settings/fetch-reservation-settings/ ${process.env.SHOP_ID}`
+          `https://shopadmin.vgrex.com/settings/fetch-reservation-settings/${process.env.SHOP_ID}`
         );
         const json = await response.json();
         setTableReservationSettings(json?.data);
