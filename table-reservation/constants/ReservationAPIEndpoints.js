2dd7dde9255cd26dbe72ac772b21025750e594ee
sambathFerns <sambath.ferns@gmail.com>
2025-10-30 12:27:52 +0530

    empty commit

--- table-reservation/constants/ReservationAPIEndpoints.js
+++
@@ -1,6 +0,0 @@
-"use client";
-export const ReservationAPIEndpoints = {
-  sendReservationOTP: "user/diningtable/reservation/sendotp",
-  shopTiming: "timing",
-  newReservation: "user/diningtable/reservation/new",
-};
